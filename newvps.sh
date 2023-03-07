07f09d0127e2f5677b65d48117cc06712dc7a9b7
Mark L Ma <marklma@users.noreply.github.com>
2023-03-07 23:11:52 +0800

    Update and rename newvps.bash to newvps.sh

--- newvps.sh
+++ newvps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/marklma/learntousevps/raw/main/sources.list
+wget https://github.com/marklma/newvps/raw/main/sources.list
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo mv ./sources.list /etc/apt/sources.list
 sudo apt-get update
@@ -16,3 +16,8 @@ sudo apt-get install lxde-core -y
 sudo apt-get install lxde -y
 sudo apt-get install xrdp -y
 sudo apt-get purge xterm
+sudo apt-get install language-pack-zh-hans -y
+sudo apt-get install fonts-arphic-bkai00mp fonts-arphic-bsmi00lp fonts-arphic-gbsn00lp fonts-arphic-ukai -y
+
+pip install shadowsocks
+sudo sed -i "s/CIPHER_CTX_cleanup/CIPHER_CTX_reset/g" /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
